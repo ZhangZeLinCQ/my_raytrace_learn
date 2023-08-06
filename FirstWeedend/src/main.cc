9e327ee77a92558a489ec4d5204e6ee0ae99128e
辞穷 <zhangzelinlearn@outlook.com>
2023-08-06 15:51:15 +0800

    10.4 Schlick全反射近似判断 10.5 空心玻璃球

--- FirstWeedend/src/main.cc
+++ FirstWeedend/src/main.cc
@@ -54,6 +54,7 @@ int main() {
   world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
   world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
   world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left)); // 反向物体，与正向包围起来形成空心玻璃球
   world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
   // Camera
--- FirstWeedend/src/material.h
+++ FirstWeedend/src/material.h
@@ -71,7 +71,7 @@ public:
     bool cannot_refract = refraction_ratio * sin_theta > 1.0; // >1 表示发生全反射，不产生折射
     vec3 direction;
 
-    if (cannot_refract)
+    if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
       direction = reflect(unit_direction, rec.normal);
     else
       direction = refract(unit_direction, rec.normal, refraction_ratio);
@@ -82,5 +82,13 @@ public:
 
 public:
   double ir; // Index of Refraction 折射率
+
+private:
+  static double reflectance(double cosine, double ref_idx) { // Christophe Schlick方法：处理Fresnel效应
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+  }
 };
 #endif
